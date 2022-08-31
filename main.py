5415dc2415b89f049f683cadfa00f7442aa6f75a
YaN9-Y <40831733+YaN9-Y@users.noreply.github.com>
2022-08-31 08:27:16 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,154 @@
+import os
+import cv2
+import random
+import numpy as np
+import torch
+import argparse
+from shutil import copyfile
+from src.config import Config
+from src.D4 import D4
+
+
+def main(mode=None):
+    r"""starts the model
+
+    Args:
+        mode (int): 1: train, 2: test, 3: eval, reads from config file if not specified
+    """
+
+    config = load_config(mode)
+
+
+    # cuda visble devices
+    os.environ['CUDA_VISIBLE_DEVICES'] = ','.join(str(e) for e in config.GPU)
+
+
+    # init device
+    if torch.cuda.is_available():
+        config.DEVICE = torch.device("cuda")
+        torch.backends.cudnn.benchmark = True   # cudnn auto-tuner
+    else:
+        config.DEVICE = torch.device("cpu")
+
+
+
+    # set cv2 running threads to 1 (prevents deadlocks with pytorch dataloader)
+    cv2.setNumThreads(0)
+
+
+    # initialize random seed
+    torch.manual_seed(config.SEED)
+    torch.cuda.manual_seed_all(config.SEED)
+    np.random.seed(config.SEED)
+    random.seed(config.SEED)
+
+
+
+    # build the model and initialize
+    model = D4(config)
+    model.load()
+
+
+    # model training
+    if config.MODE == 1:
+        config.print()
+        print('\nstart training...\n')
+        model.train()
+
+    # model test
+    elif config.MODE == 2:
+        print('\nstart testing...\n')
+        model.test()
+
+    # eval mode
+    else:
+        print('\nstart eval...\n')
+        model.eval()
+
+
+def load_config(mode=None):
+    r"""loads model config
+
+    Args:
+        mode (int): 1: train, 2: test, 3: eval, reads from config file if not specified
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', '--checkpoints', type=str, default='./checkpoints', help='model checkpoints path (default: ./checkpoints)')
+    parser.add_argument('--model', type=int, choices=[1, 2, 3, 4, 5, 6, 7], help='1: edge model, 2: inpaint model, 3: edge-inpaint model, 4: joint model')
+
+    # test mode
+    if mode == 2:
+        parser.add_argument('--input', type=str, help='path to the input images directory or an input image')
+        parser.add_argument('--output', type=str, help='path to the output directory')
+        parser.add_argument('--crop', type=bool)
+        parser.add_argument('--crop_size', type=int, nargs=2)
+
+    args = parser.parse_args()
+    config_path = os.path.join(args.path, 'config.yml')
+
+    # create checkpoints path if doesn't exist
+    if not os.path.exists(args.path):
+        os.makedirs(args.path)
+
+    # copy config template if doesn't exist
+    if not os.path.exists(config_path):
+        copyfile('./config.yml.example', config_path)
+
+    # load config file
+    config = Config(config_path)
+
+    # train mode
+    if mode == 1:
+        config.MODE = 1
+        if args.model:
+            config.MODEL = args.model
+
+        if config.DATASET == 'ITS':
+            config.USE_DC_A = 0
+            config.USE_GUIDED_FILTER = 0
+            config.USE_DCP = 0
+        else:
+            config.USE_DC_A = 1
+            config.USE_GUIDED_FILTER = 0
+            config.USE_DCP = 1
+
+    # test mode
+    elif mode == 2:
+        config.MODE = 2
+        config.MODEL = args.model if args.model is not None else 3
+        config.INPUT_SIZE = 0
+
+        if args.input is not None:
+            config.TEST_FLIST = args.input
+
+        if args.output is not None:
+            config.RESULTS = args.output
+
+        if args.crop is not None and args.crop_size is not None:
+            config.CROP = args.crop
+            config.CROP_SIZE = args.crop_size
+
+        if config.DATASET == 'SOTS-indoor':
+            config.USE_DC_A = 0
+            config.USE_GUIDED_FILTER = 0
+        elif config.DATASET == 'Foggycityscape':
+            config.USE_DC_A = 1
+            config.USE_GUIDED_FILTER = 0
+        elif config.DATASET == 'SOTS-outdoor':
+            config.USE_DC_A = 1
+            config.USE_GUIDED_FILTER = 1
+        elif config.DATASET == 'IHAZE':
+            config.USE_DC_A = 1
+            config.USE_GUIDED_FILTER = 0
+            config.TIMES = 0.7
+        elif config.DATASET == 'REAL' or config.DATASET == 'RTTS':
+            config.USE_DC_A = 1
+            config.USE_GUIDED_FILTER = 1   #set this to 0 for higher FADE, may produce artifacts.
+
+
+    return config
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/loss.py
@@ -0,0 +1,498 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+from math import exp
+
+from torch.nn.modules.utils import _pair
+
+
+class Vgg19(torch.nn.Module):
+    def __init__(self, requires_grad=False):
+        super(Vgg19, self).__init__()
+        vgg_pretrained_features = models.vgg19(pretrained=True).features
+        self.slice1 = torch.nn.Sequential()
+        self.slice2 = torch.nn.Sequential()
+        self.slice3 = torch.nn.Sequential()
+        self.slice4 = torch.nn.Sequential()
+        self.slice5 = torch.nn.Sequential()
+        for x in range(2):
+            self.slice1.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(2, 7):
+            self.slice2.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(7, 12):
+            self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(12, 21):
+            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(21, 30):
+            self.slice5.add_module(str(x), vgg_pretrained_features[x])
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
+
+    def forward(self, X):
+        h_relu1 = self.slice1(X)
+        h_relu2 = self.slice2(h_relu1)
+        h_relu3 = self.slice3(h_relu2)
+        h_relu4 = self.slice4(h_relu3)
+        h_relu5 = self.slice5(h_relu4)
+        return [h_relu1, h_relu2, h_relu3, h_relu4, h_relu5]
+
+
+
+
+class ContrastLoss(nn.Module):
+    def __init__(self, ablation=False):
+
+        super(ContrastLoss, self).__init__()
+        self.vgg = Vgg19().cuda()
+        self.l1 = nn.L1Loss()
+        self.weights = [0, 0.4, 0.6, 0, 1.0]
+        self.ab = ablation
+
+    def forward(self, a, p, n):
+        a_vgg, p_vgg, n_vgg = self.vgg(a), self.vgg(p), self.vgg(n)
+        loss = 0
+
+        d_ap, d_an = 0, 0
+        for i in range(len(a_vgg)):
+            d_ap = self.l1(a_vgg[i], p_vgg[i])
+            if not self.ab:
+                d_an = self.l1(a_vgg[i], n_vgg[i])
+                contrastive = d_ap / (d_an + 1e-7)
+            else:
+                contrastive = d_ap
+
+            loss += self.weights[i] * contrastive
+        return loss
+
+
+
+
+class AdversarialLoss(nn.Module):
+    r"""
+    Adversarial loss
+    https://arxiv.org/abs/1711.10337
+    """
+
+    def __init__(self, type='nsgan', target_real_label=1.0, target_fake_label=0.0):
+        r"""
+        type = nsgan | lsgan | hinge
+        """
+        super(AdversarialLoss, self).__init__()
+
+        self.type = type
+        self.register_buffer('real_label', torch.tensor(target_real_label))
+        self.register_buffer('fake_label', torch.tensor(target_fake_label))
+
+        if type == 'nsgan':
+            self.criterion = nn.BCELoss()
+
+        elif type == 'lsgan':
+            self.criterion = nn.MSELoss()
+
+        elif type == 'hinge':
+            self.criterion = nn.ReLU()
+
+    def __call__(self, outputs, is_real, is_disc=None):
+        if self.type == 'hinge':
+            if is_disc:
+                if is_real:
+                    outputs = -outputs
+                return self.criterion(1 + outputs).mean()
+            else:
+                return (-outputs).mean()
+
+        else:
+            labels = (self.real_label if is_real else self.fake_label).expand_as(outputs)
+            loss = self.criterion(outputs, labels)
+            return loss
+
+
+
+class StyleLoss(nn.Module):
+    r"""
+    Perceptual loss, VGG-based
+    https://arxiv.org/abs/1603.08155
+    https://github.com/dxyang/StyleTransfer/blob/master/utils.py
+    """
+
+    def __init__(self):
+        super(StyleLoss, self).__init__()
+        self.add_module('vgg', VGG19())
+        self.criterion = torch.nn.L1Loss()
+
+    def compute_gram(self, x):
+        b, ch, h, w = x.size()
+        f = x.view(b, ch, w * h)
+        f_T = f.transpose(1, 2)
+        G = f.bmm(f_T) / (h * w * ch)
+
+        return G
+
+    def __call__(self, x, y):
+        # Compute features
+        x_vgg, y_vgg = self.vgg(x), self.vgg(y)
+
+        # Compute loss
+        style_loss = 0.0
+        style_loss += self.criterion(self.compute_gram(x_vgg['relu2_2']), self.compute_gram(y_vgg['relu2_2']))
+        style_loss += self.criterion(self.compute_gram(x_vgg['relu3_4']), self.compute_gram(y_vgg['relu3_4']))
+        style_loss += self.criterion(self.compute_gram(x_vgg['relu4_4']), self.compute_gram(y_vgg['relu4_4']))
+        style_loss += self.criterion(self.compute_gram(x_vgg['relu5_2']), self.compute_gram(y_vgg['relu5_2']))
+
+        return style_loss
+
+class DecoderStyleLoss(nn.Module):
+    def __init__(self):
+        super(DecoderStyleLoss, self).__init__()
+        self.criterion = nn.L1Loss()
+
+    def compute_gram(self, x):
+        b, ch, h, w = x.size()
+        f = x.view(b, ch, w * h)
+        f_T = f.transpose(1, 2)
+        G = f.bmm(f_T) / (h * w * ch)
+
+        return G
+
+    def __call__(self, x, y, masks):
+        assert len(x) == len(y)
+        style_loss = 0.
+
+        for i in range(len(x)):
+            H,W = x[i].shape[2:4]
+            scaled_masks = torch.nn.functional.interpolate(masks,[H,W])
+            style_loss += self.criterion(self.compute_gram(x[i]*scaled_masks),self.compute_gram(y[i]*scaled_masks))
+
+        return style_loss
+
+
+class PerceptualLoss(nn.Module):
+    r"""
+    Perceptual loss, VGG-based
+    https://arxiv.org/abs/1603.08155
+    https://github.com/dxyang/StyleTransfer/blob/master/utils.py
+    """
+
+    def __init__(self, weights=[1.0, 1.0, 1.0, 1.0, 1.0]):
+        super(PerceptualLoss, self).__init__()
+        self.add_module('vgg', VGG19())
+        self.criterion = torch.nn.L1Loss()
+        self.weights = weights
+
+    def __call__(self, x, y):
+        # Compute features
+        x_vgg, y_vgg = self.vgg(x), self.vgg(y)
+
+        content_loss = 0.0
+        content_loss += self.weights[0] * self.criterion(x_vgg['relu1_1'], y_vgg['relu1_1'])
+        content_loss += self.weights[1] * self.criterion(x_vgg['relu2_1'], y_vgg['relu2_1'])
+        content_loss += self.weights[2] * self.criterion(x_vgg['relu3_1'], y_vgg['relu3_1'])
+        content_loss += self.weights[3] * self.criterion(x_vgg['relu4_1'], y_vgg['relu4_1'])
+        content_loss += self.weights[4] * self.criterion(x_vgg['relu5_1'], y_vgg['relu5_1'])
+
+
+        return content_loss
+
+
+
+class VGG19(torch.nn.Module):
+    def __init__(self):
+        super(VGG19, self).__init__()
+        features = models.vgg19(pretrained=True).features
+        self.relu1_1 = torch.nn.Sequential()
+        self.relu1_2 = torch.nn.Sequential()
+
+        self.relu2_1 = torch.nn.Sequential()
+        self.relu2_2 = torch.nn.Sequential()
+
+        self.relu3_1 = torch.nn.Sequential()
+        self.relu3_2 = torch.nn.Sequential()
+        self.relu3_3 = torch.nn.Sequential()
+        self.relu3_4 = torch.nn.Sequential()
+
+        self.relu4_1 = torch.nn.Sequential()
+        self.relu4_2 = torch.nn.Sequential()
+        self.relu4_3 = torch.nn.Sequential()
+        self.relu4_4 = torch.nn.Sequential()
+
+        self.relu5_1 = torch.nn.Sequential()
+        self.relu5_2 = torch.nn.Sequential()
+        self.relu5_3 = torch.nn.Sequential()
+        self.relu5_4 = torch.nn.Sequential()
+
+        for x in range(2):
+            self.relu1_1.add_module(str(x), features[x])
+
+        for x in range(2, 4):
+            self.relu1_2.add_module(str(x), features[x])
+
+        for x in range(4, 7):
+            self.relu2_1.add_module(str(x), features[x])
+
+        for x in range(7, 9):
+            self.relu2_2.add_module(str(x), features[x])
+
+        for x in range(9, 12):
+            self.relu3_1.add_module(str(x), features[x])
+
+        for x in range(12, 14):
+            self.relu3_2.add_module(str(x), features[x])
+
+        for x in range(14, 16):
+            self.relu3_3.add_module(str(x), features[x])
+
+        for x in range(16, 18):
+            self.relu3_4.add_module(str(x), features[x])
+
+        for x in range(18, 21):
+            self.relu4_1.add_module(str(x), features[x])
+
+        for x in range(21, 23):
+            self.relu4_2.add_module(str(x), features[x])
+
+        for x in range(23, 25):
+            self.relu4_3.add_module(str(x), features[x])
+
+        for x in range(25, 27):
+            self.relu4_4.add_module(str(x), features[x])
+
+        for x in range(27, 30):
+            self.relu5_1.add_module(str(x), features[x])
+
+        for x in range(30, 32):
+            self.relu5_2.add_module(str(x), features[x])
+
+        for x in range(32, 34):
+            self.relu5_3.add_module(str(x), features[x])
+
+        for x in range(34, 36):
+            self.relu5_4.add_module(str(x), features[x])
+
+        # don't need the gradients, just want the features
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def forward(self, x):
+        relu1_1 = self.relu1_1(x)
+        relu1_2 = self.relu1_2(relu1_1)
+
+        relu2_1 = self.relu2_1(relu1_2)
+        relu2_2 = self.relu2_2(relu2_1)
+
+        relu3_1 = self.relu3_1(relu2_2)
+        relu3_2 = self.relu3_2(relu3_1)
+        relu3_3 = self.relu3_3(relu3_2)
+        relu3_4 = self.relu3_4(relu3_3)
+
+        relu4_1 = self.relu4_1(relu3_4)
+        relu4_2 = self.relu4_2(relu4_1)
+        relu4_3 = self.relu4_3(relu4_2)
+        relu4_4 = self.relu4_4(relu4_3)
+
+        relu5_1 = self.relu5_1(relu4_4)
+        relu5_2 = self.relu5_2(relu5_1)
+        relu5_3 = self.relu5_3(relu5_2)
+        relu5_4 = self.relu5_4(relu5_3)
+
+        out = {
+            'relu1_1': relu1_1,
+            #'relu1_2': relu1_2,
+
+            'relu2_1': relu2_1,
+            'relu2_2': relu2_2,
+
+            'relu3_1': relu3_1,
+            #'relu3_2': relu3_2,
+            #'relu3_3': relu3_3,
+            'relu3_4': relu3_4,
+
+            'relu4_1': relu4_1,
+            #'relu4_2': relu4_2,
+            #'relu4_3': relu4_3,
+            'relu4_4': relu4_4,
+
+            'relu5_1': relu5_1,
+            'relu5_2': relu5_2,
+            #'relu5_3': relu5_3,
+            #'relu5_4': relu5_4,
+        }
+        return out
+
+
+class DecoderFeatureLoss(nn.Module):
+    def __init__(self):
+        super(DecoderFeatureLoss, self).__init__()
+        self.criterion = nn.L1Loss()
+
+    def __call__(self, x, y, weight=[1.0,1.0,1.0,1.0,1.0]): #convconv64_256, deconv64_128_128,conv128_128,deconv128_256_64,conv256_3
+        assert len(weight) == 5 and len(x) == len(y)
+        decoder_feature_loss = 0.
+        for i in range(len(x)):
+            decoder_feature_loss += weight[i] * self.criterion(x[i],y[i])
+
+        return decoder_feature_loss
+
+
+
+
+
+#############  ssim loss ##########33
+def gaussian(window_size, sigma):
+    gauss = torch.Tensor([exp(-(x - window_size//2)**2/float(2*sigma**2)) for x in range(window_size)])
+    return gauss/gauss.sum()
+
+
+def create_window(window_size, channel=1):
+    _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
+    _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+    window = _2D_window.expand(channel, 1, window_size, window_size).contiguous()
+    return window
+
+
+class TVLoss(nn.Module):
+    def __init__(self):
+        super(TVLoss,self).__init__()
+
+    def forward(self, x):
+        height = x.size()[2]
+        width = x.size()[3]
+        tv_h = torch.div(torch.sum(torch.abs(x[:,:,1:,:] - x[:,:,:-1,:])),(x.size()[0]*x.size()[1]*(height-1)*width))
+        tv_w = torch.div(torch.sum(torch.abs(x[:,:,:,1:] - x[:,:,:,:-1])),(x.size()[0]*x.size()[1]*(height)*(width-1)))
+        return tv_w + tv_h
+    
+
+class WeightedL1Loss(nn.Module):
+    def __init__(self):
+        super(WeightedL1Loss, self).__init__()
+
+    def forward(self, f_pred, f_gt):
+        weights = self.cal_weight(f_gt)
+        loss = torch.mean(torch.sum(weights*(torch.mean(torch.mean(torch.abs(f_pred-f_gt), dim=3, keepdim=True),dim=2,keepdim=True)),dim=1))
+
+        #loss = torch.mean(torch.abs(weights*(f_pred-f_gt)))
+
+        return loss
+
+
+
+
+    def cal_weight(self, f):
+        f_mean = torch.mean(f, dim=1, keepdim=True)
+        sigma = torch.sqrt(torch.sum(torch.sum((f - f_mean) ** 2, dim=3, keepdim=True), dim=2, keepdim=True))
+        r = torch.sum(torch.sum(f - f_mean, dim=3, keepdim=True), dim=2, keepdim=True) / sigma
+
+        weights = torch.nn.functional.softmax(r).detach()
+
+        return weights
+
+class HazeReconstructionLoss(nn.Module):
+    def __init__(self):
+        super(HazeReconstructionLoss, self).__init__()
+        self.criterion = nn.L1Loss()
+
+    def forward(self, pred_transmission, pred_features, hazy_features, hazy_imgs):
+        A = self.get_A(hazy_features)
+        reconstructed_hazy_features = pred_features * pred_transmission + A * (1-pred_transmission)
+        return self.criterion(reconstructed_hazy_features, hazy_features)
+
+
+    def get_A(self, hazy_features):
+        n,c,h,w = hazy_features.shape
+        dark_channel = torch.min(hazy_features, dim=1)[0]
+        A_index_unfold = torch.max(dark_channel.reshape(n,h*w),dim=1)[1]
+        # print(A_index_unfold.shape)
+        # print(A_index_unfold.unsqueeze(0).transpose(1, 0).unsqueeze(1).repeat(1, c, 1).shape)
+        A = torch.gather(hazy_features.reshape(n,c,h*w), 2, A_index_unfold.unsqueeze(0).transpose(1,0).unsqueeze(1).repeat(1,c,1)).unsqueeze(3)
+
+        # print(A.shape)
+        return A
+
+
+class Get_gradient(nn.Module):
+    def __init__(self):
+        super(Get_gradient, self).__init__()
+        kernel_v = [[0, -1, 0],
+                    [0, 0, 0],
+                    [0, 1, 0]]
+        kernel_h = [[0, 0, 0],
+                    [-1, 0, 1],
+                    [0, 0, 0]]
+        kernel_h = torch.FloatTensor(kernel_h).unsqueeze(0).unsqueeze(0)
+        kernel_v = torch.FloatTensor(kernel_v).unsqueeze(0).unsqueeze(0)
+        self.weight_h = nn.Parameter(data = kernel_h, requires_grad = False).cuda()
+        self.weight_v = nn.Parameter(data = kernel_v, requires_grad = False).cuda()
+
+    def forward(self, x):
+        x0 = x[:, 0]
+        x1 = x[:, 1]
+        x2 = x[:, 2]
+        x0_v = F.conv2d(x0.unsqueeze(1), self.weight_v, padding=2)
+        x0_h = F.conv2d(x0.unsqueeze(1), self.weight_h, padding=2)
+
+        x1_v = F.conv2d(x1.unsqueeze(1), self.weight_v, padding=2)
+        x1_h = F.conv2d(x1.unsqueeze(1), self.weight_h, padding=2)
+
+        x2_v = F.conv2d(x2.unsqueeze(1), self.weight_v, padding=2)
+        x2_h = F.conv2d(x2.unsqueeze(1), self.weight_h, padding=2)
+
+        x0 = torch.sqrt(torch.pow(x0_v, 2) + torch.pow(x0_h, 2) + 1e-6)
+        x1 = torch.sqrt(torch.pow(x1_v, 2) + torch.pow(x1_h, 2) + 1e-6)
+        x2 = torch.sqrt(torch.pow(x2_v, 2) + torch.pow(x2_h, 2) + 1e-6)
+
+        x = torch.cat([x0, x1, x2], dim=1)
+        return x
+
+class Get_gradient_nopadding(nn.Module):
+    def __init__(self):
+        super(Get_gradient_nopadding, self).__init__()
+        kernel_v = [[0, -1, 0],
+                    [0, 0, 0],
+                    [0, 1, 0]]
+        kernel_h = [[0, 0, 0],
+                    [-1, 0, 1],
+                    [0, 0, 0]]
+        kernel_h = torch.FloatTensor(kernel_h).unsqueeze(0).unsqueeze(0)
+        kernel_v = torch.FloatTensor(kernel_v).unsqueeze(0).unsqueeze(0)
+        self.weight_h = nn.Parameter(data = kernel_h, requires_grad = False).cuda()
+        self.weight_v = nn.Parameter(data = kernel_v, requires_grad = False).cuda()
+
+    def forward(self, x):
+        x0 = x[:, 0]
+        x1 = x[:, 1]
+        x2 = x[:, 2]
+        x0_v = F.conv2d(x0.unsqueeze(1), self.weight_v, padding = 1)
+        x0_h = F.conv2d(x0.unsqueeze(1), self.weight_h, padding = 1)
+
+        x1_v = F.conv2d(x1.unsqueeze(1), self.weight_v, padding = 1)
+        x1_h = F.conv2d(x1.unsqueeze(1), self.weight_h, padding = 1)
+
+        x2_v = F.conv2d(x2.unsqueeze(1), self.weight_v, padding = 1)
+        x2_h = F.conv2d(x2.unsqueeze(1), self.weight_h, padding = 1)
+
+        x0 = torch.sqrt(torch.pow(x0_v, 2) + torch.pow(x0_h, 2) + 1e-6)
+        x1 = torch.sqrt(torch.pow(x1_v, 2) + torch.pow(x1_h, 2) + 1e-6)
+        x2 = torch.sqrt(torch.pow(x2_v, 2) + torch.pow(x2_h, 2) + 1e-6)
+
+        x = torch.cat([x0, x1, x2], dim=1)
+        return x
+
+
+#Both rgb2hsv and hsv2rgb take images in a mini-batch of images (NCHW format) as input.
+# The range of H (hue) is from 0 to 360 (the outside of the range loops). The range of RGB and SV is 0 to 1.
+
+def soft_pool2d(x, kernel_size=2, stride=None, force_inplace=False):
+    kernel_size = _pair(kernel_size)
+    if stride is None:
+        stride = kernel_size
+    else:
+        stride = _pair(stride)
+    # Get input sizes
+    _, c, h, w = x.size()
+    # Create per-element exponential value sum : Tensor [b x 1 x h x w]
+    e_x = torch.sum(torch.exp(x),dim=1,keepdim=True)
+    # Apply mask to input and pool and calculate the exponential sum
+    # Tensor: [b x c x h x w] -> [b x c x h' x w']
+    return F.avg_pool2d(x.mul(e_x), kernel_size, stride=stride).mul_(sum(kernel_size)).div_(F.avg_pool2d(e_x, kernel_size, stride=stride).mul_(sum(kernel_size)))
